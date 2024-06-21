060f58d8926f7f10d17726e06bf783497e9bd749
Akash Panchal <akash@workbook.local>
2024-06-21 18:39:02 +0530

    Make the year in footer dynamic

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+function getYear() {
+  return new Date().getFullYear();
+}
+
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Akash Panchal. All rights reserved.
+        &copy; {getYear()} Akash Panchal. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
