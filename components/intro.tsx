5b6f5d0421ba27469295cec406f44ab6533e2ef5
Akash Panchal <akashp1712@gmail.com>
2025-05-11 13:33:50 +0530

    Removing read.cv profile link

--- components/intro.tsx
+++ components/intro.tsx
@@ -146,13 +146,6 @@ export default function Intro() {
           <BsLinkedin />
         </a>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://read.cv/akashpanchal"
-          target="_blank"
-        >
-          <PiReadCvLogoFill />
-        </a>
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
